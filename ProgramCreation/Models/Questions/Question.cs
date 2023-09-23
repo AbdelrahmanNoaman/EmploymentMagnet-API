84a753f693f598b04c3fd89c475002d5c74cbb62
Nu3man11 <76150639+AbdelrahmanNoaman@users.noreply.github.com>
2023-09-23 15:51:13 +0300

    Adding Base Class Question + ShortAnswerQuestion

---
+++ ProgramCreation/Models/Questions/Question.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgramCreation.Models.Questions
+{
+    abstract class Question
+    {
+        private string _id;
+        private string _title;
+        private string _type;
+        private string _sectionName;
+        private bool? _isHidden;
+        private bool? _isMandatory;
+        private bool? _isInternal;
+
+        public string Id
+        {
+            get { return this._id; }
+            set { this._id = value; }
+        }
+
+        public string Title
+        {
+            get { return this._title; }
+            set { this._title = value; }
+        }
+
+        public string Type
+        {
+            get { return this._type; }
+            set { this._type = value; }
+        }
+
+        public string SectionName
+        {
+            get { return this._sectionName; }
+            set { this._sectionName = value; }
+        }
+
+        public bool? IsHidden
+        {
+            get { return this._isHidden; }
+            set { this._isHidden = value; }
+        }
+
+        public bool? IsInternal
+        {
+            get { return this._isInternal; }
+            set { this._isInternal = value; }
+        }
+
+        public bool? IsMandatory
+        {
+            get { return this._isMandatory; }
+            set { this._isMandatory = value; }
+        }
+
+        public Question(string id, string title, string sectionName)
+        {
+            this.Id = id;
+            this.Title = title;
+            this.SectionName = sectionName;
+            this.IsHidden = false;
+            switch (this.Title)
+            {
+                case "Personal Information":
+                    this.IsInternal = false; break;
+                case "Profile":
+                    this.IsMandatory = false; break;
+                default:
+                    break;
+            }
+        }
+    }
+}
---
+++ ProgramCreation/Models/Questions/ShortAnswerQuestion.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgramCreation.Models.Questions
+{
+    internal class ShortAnswerQuestion:Question
+    {
+        public ShortAnswerQuestion(string id, string title, string sectionName) : base(id, title, sectionName)
+        {
+            this.Type = "Short answer";
+        }
+    }
+}
