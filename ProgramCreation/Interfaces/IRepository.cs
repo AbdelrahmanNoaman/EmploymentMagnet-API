ce19ba4badc6248b8f2fb920cbd79e608eda1549
Nu3man11 <76150639+AbdelrahmanNoaman@users.noreply.github.com>
2023-09-23 16:45:10 +0300

    Adding Repository Interface

---
+++ ProgramCreation/Interfaces/IRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgramCreation.Interfaces
+{
+    internal interface IRepository<T>
+    {
+        T GetById(int id);
+        IEnumerable<T> GetAll();
+        void Add(T element);
+        void Delete(T element);
+    }
+}
