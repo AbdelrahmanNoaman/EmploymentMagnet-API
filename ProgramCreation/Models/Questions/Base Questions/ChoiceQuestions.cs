c08a57bda1b6211245a00d7bcbb1784192a6c422
Nu3man11 <76150639+AbdelrahmanNoaman@users.noreply.github.com>
2023-09-23 15:55:31 +0300

    Adding Base Class ChoiceQuestions, and Derived class DateQuestion

---
+++ ProgramCreation/Models/Questions/Base Questions/ChoiceQuestions.cs
@@ -0,0 +1,32 @@
+﻿using ProgramCreation.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgramCreation.Models.Questions
+{
+    abstract class ChoiceQuestion : Question, IQuestion
+    {
+        private List<string> _choices;
+        private bool _enableOther;
+
+        public List<string> Choices
+        {
+            get { return _choices; }
+            set { _choices = value; }
+        }
+
+        public bool EnableOther
+        {
+            get { return _enableOther; }
+            set { _enableOther = value; }
+        }
+        public ChoiceQuestion(string id, string title, string sectionName, List<string> choices, bool enableOther) : base(id, title, sectionName)
+        {
+            this.Choices = choices;
+            this.EnableOther = enableOther;
+        }
+    }
+}
---
+++ ProgramCreation/Models/Questions/DateQuestion.cs
@@ -0,0 +1,17 @@
+﻿using ProgramCreation.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgramCreation.Models.Questions
+{
+    internal class DateQuestion : Question, IQuestion
+    {
+        public DateQuestion(string id, string title, string sectionName) : base(id, title, sectionName)
+        {
+            this.Type = "Date";
+        }
+    }
+}
