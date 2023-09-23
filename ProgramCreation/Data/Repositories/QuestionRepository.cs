31e9663828b328b8f0e8adfb6e296746d7b32c16
Nu3man11 <76150639+AbdelrahmanNoaman@users.noreply.github.com>
2023-09-23 16:48:33 +0300

    Adding Question Info DTO

--- ProgramCreation/Data/Repositories/QuestionRepository.cs
+++ ProgramCreation/Data/Repositories/QuestionRepository.cs
@@ -1,12 +1,19 @@
-﻿using System;
+﻿using ProgramCreation.Models.Questions;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ProgramCreation.Interfaces;
+using ProgramCreation.DTOs;
 
 namespace ProgramCreation.Data.Repositories
 {
-    internal class QuestionRepository
+    internal class QuestionRepository:IRepository<QuestionDTO>
     {
+        public static QuestionDTO GetById(string id)
+        {
+
+        }
     }
 }
--- ProgramCreation/Interfaces/IRepository.cs
+++ ProgramCreation/Interfaces/IRepository.cs
@@ -8,7 +8,6 @@ namespace ProgramCreation.Interfaces
 {
     internal interface IRepository<T>
     {
-        T GetById(int id);
         IEnumerable<T> GetAll();
         void Add(T element);
         void Delete(T element);
