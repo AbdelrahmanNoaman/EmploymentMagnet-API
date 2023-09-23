c78abb40692d4295e89df9c9c98e23c19f29077b
Nu3man11 <76150639+AbdelrahmanNoaman@users.noreply.github.com>
2023-09-24 00:46:16 +0300

    Finishing Stage Repository

--- ProgramCreation/DTOs/StageDTO.cs
+++ ProgramCreation/DTOs/StageDTO.cs
@@ -9,15 +9,15 @@ namespace ProgramCreation.DTOs
 {
     public class StageDTO
     {
-        public string Id { get; set; }
+        public string? id { get; set; }
         public string Name { get; set; }
         public string Type { get; set; }
         public bool IsShown { get; set; }
-        public string InterviewQuestion { get; set; }
-        public string AdditionalInfo { get;  set ; }
-        public int MaxDuration { get; set; }
-        public TimeEnum TimeIn { get; set ; }
-        public int NumberOfDaysLeft { get; set ; }
+        public string? InterviewQuestion { get; set; }
+        public string? AdditionalInfo { get;  set ; }
+        public int? MaxDuration { get; set; }
+        public TimeEnum? TimeIn { get; set ; }
+        public int? NumberOfDaysLeft { get; set ; }
 
     }
 }
--- ProgramCreation/Data/Repositories/StagesRepository.cs
+++ ProgramCreation/Data/Repositories/StagesRepository.cs
@@ -4,50 +4,28 @@ using ProgramCreation.Models.Stages.Base_Stages;
 using Microsoft.Azure.Cosmos;
 namespace ProgramCreation.Data.Repositories
 {
-    public class StagesRepository:IRepository<Stage,StageInfoDTO>
+    public class StagesRepository:IRepository<StageDTO,StageInfoDTO>
     {
         private Microsoft.Azure.Cosmos.Container _container = new DbContext().GetContainer("stages");
 
-        public async Task<Stage> GetById(StageInfoDTO StageInfo)
+        public async Task<StageDTO> GetById(StageInfoDTO StageInfo)
         {
 
-            var result = await _container.ReadItemAsync<Stage>(StageInfo.id, new PartitionKey(StageInfo.Type));
+            var result = await _container.ReadItemAsync<StageDTO>(StageInfo.id, new PartitionKey(StageInfo.Type));
             return result.Resource;
         }
 
-        public async Task<QuestionInfoDTO> Add(Stage stage)
+        public async Task<StageInfoDTO> Add(StageDTO stage)
         {
             stage.id = Guid.NewGuid().ToString();
             //IQuestion newQues = QuestionFactory.CreateQuestion(question);
             var result = await _container.CreateItemAsync(stage);
-            return new QuestionInfoDTO { id = stage.id, Type = stage.Type };
+            return new StageInfoDTO { id = stage.id, Type = stage.Type };
         }
 
-        public async Task Delete(QuestionInfoDTO questionInfo)
+        public async Task Delete(StageInfoDTO questionInfo)
         {
-            var result = await _container.DeleteItemAsync<Object>(questionInfo.id, new PartitionKey(questionInfo.Type));
-        }
-
-        public async Task ChangeQuestionHiddenState(QuestionInfoDTO questionInfo, bool state)
-        {
-            QuestionDTO question = await this.GetById(questionInfo);
-            question.IsHidden = state;
-            var finalResult = await _container.ReplaceItemAsync<QuestionDTO>(question, question.id, new PartitionKey(question.Type));
-        }
-
-        public async Task ChangeQuestionInternalState(QuestionInfoDTO questionInfo, bool state)
-        {
-            QuestionDTO question = await this.GetById(questionInfo);
-            question.IsInternal = state;
-            var finalResult = await _container.ReplaceItemAsync<QuestionDTO>(question, question.id, new PartitionKey(question.Type));
-
-        }
-        public async Task ChangeQuestionMandatoryState(QuestionInfoDTO questionInfo, bool state)
-        {
-            QuestionDTO question = await this.GetById(questionInfo);
-            question.IsMandatory = state;
-            var finalResult = await _container.ReplaceItemAsync<QuestionDTO>(question, question.id, new PartitionKey(question.Type));
-
+           await _container.DeleteItemAsync<Object>(questionInfo.id, new PartitionKey(questionInfo.Type));
         }
     }
 }
